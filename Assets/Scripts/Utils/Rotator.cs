163720d06423856a01444bea82815a14b00b4ada
Luca Neukom <luca.neukom@gmail.com>
2022-12-09 15:36:30 +0100

    add randomization to Rotator

--- Assets/Scripts/Utils/Rotator.cs
+++ Assets/Scripts/Utils/Rotator.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class Rotator : MonoBehaviour {
     [SerializeField]
     private Vector3 rotationSpeed;
+    [SerializeField]
+    private Vector3 randomizationFactors;
+
+    public Vector3 randomization;
+
+    void Start() {
+        Vector3 random = Random.insideUnitSphere - new Vector3(0.5f, 0.5f, 0.5f);
+        randomization = Vector3.Scale(random, randomizationFactors);
+    }
 
     void Update() {
-        Vector3 toRotate = rotationSpeed * Time.deltaTime;
-        transform.Rotate(toRotate, Space.World);
+        Vector3 toRotate = rotationSpeed + Vector3.Scale(rotationSpeed, randomization);
+        transform.Rotate(toRotate * Time.deltaTime, Space.World);
     }
 }
