15f617f8fbb4ef30df9a7a79a5d516563de89e74
Luca Neukom <luca.neukom@gmail.com>
2023-03-31 13:57:33 +0200

    implement dropping and automatic return of resources

--- Assets/Scripts/Player/Resources/Resource.cs
+++ Assets/Scripts/Player/Resources/Resource.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 
 public class Resource : MonoBehaviour, Cargo {
+    private static float DroppingRange = 5;
+    private static float FloatSpeed = 2;
+    private static float ReturnDelay = 5;
+
     private bool initialized = false;
     private ResourceType type = null;
     private ResourceSpawnLocation resourceSpawnLocation = null;
     private ResourceSpawner resourceSpawner = null;
 
+    private TweenerCore<Vector3, Vector3, VectorOptions> returnTween = null;
+
     public void Initialize(ResourceType type, ResourceSpawnLocation startingPosition, ResourceSpawner resourceSpawner) {
         if (!initialized) {
             this.type = type;
             resourceSpawnLocation = startingPosition;
-            resourceSpawner = resourceSpawner;;
+            this.resourceSpawner = resourceSpawner;
             initialized = true;
             startingPosition.PlaceResource(this);
         } else {
@@ -19,32 +28,45 @@ public class Resource : MonoBehaviour, Cargo {
     }
 
     public void AttachToTransform(Transform attachmentPoint) {
-        resourceSpawnLocation.TakeResource();
-        resourceSpawnLocation = null;
+        if (returnTween != null) {
+            returnTween.Kill();
+            returnTween = null;
+        }
+        if (resourceSpawnLocation != null) {
+            resourceSpawnLocation.TakeResource();
+            resourceSpawnLocation = null;
+        }
         PickupHelper.Attach(this, attachmentPoint);
     }
 
     public void DetachFromTransform() {
         PickupHelper.Detach(this);
-        if (IsSpawnerInRange()) {
-            resourceSpawner.ReturnResource();
-            Destroy(gameObject);
-
-        } else if (!IsHoardInRange) {
+         if (!IsHoardInRange()) {
             MoveToSpawner();
         }
     }
 
     private bool IsHoardInRange() {
-
-    }
-
-    private bool IsSpawnerInRange() {
-
+        Hoard[] hoards = FindObjectsOfType<Hoard>();
+        foreach (var hoard in hoards) {
+            if (Vector3.Distance(transform.position, hoard.transform.position) < DroppingRange) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void MoveToSpawner() {
+        returnTween = transform.DOMove(resourceSpawner.transform.position, FloatSpeed)
+            .SetEase(Ease.InExpo)
+            .SetSpeedBased(true)
+            .SetDelay(ReturnDelay)
+            .OnComplete(ReturnToSpawner);
+    }
 
+    private void ReturnToSpawner() {
+        resourceSpawner.ReturnResource();
+        Destroy(gameObject);
     }
 
     public GameObject GetGameObject() {
