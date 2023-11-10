80f6c4a1373cdf78ecece8ce7b2281cd5143c065
Luca Neukom <luca.neukom@gmail.com>
2023-11-10 16:05:04 +0100

    style resource spawner
    - add stand model
    - add ping shader
    - add hologram shader
    - add resource display to test map

--- Assets/Scripts/Player/Resources/ResourceSpawner.cs
+++ Assets/Scripts/Player/Resources/ResourceSpawner.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class ResourceSpawner : MonoBehaviour {
-
+    public float resourceDisplayHeight;
+    public Material resourceDisplayMaterial;
 
     [field: SerializeField]
     public List<ResourceSpawnLocation> ResourceLocations { get; set; } = new List<ResourceSpawnLocation>();
@@ -16,6 +18,17 @@ public class ResourceSpawner : MonoBehaviour {
     private float timeSinceLastSpawn = 0f;
     private int amountSpawned = 0;
     private bool spawning = false;
+
+    private GameObject resourceDisplay;
+
+    public void Start() {
+        var wave = FindWaveDefinitionForRound(0);
+        if (wave.Equals(null)) {
+            clearResourceDisplay();
+        } else {
+            setupResourceDisplay(wave);
+        }
+    }
 
     public void Update() {
 
@@ -51,12 +64,35 @@ public class ResourceSpawner : MonoBehaviour {
     /// </summary>
     /// <param name="roundNumber">Round which should be prepared</param>
     internal void PrepareNewRound(int roundNumber) {
-        var wave = waves.OrderByDescending(wave => wave.RoundToSpawnIn).FirstOrDefault(wave => wave.RoundToSpawnIn <= roundNumber);
+        var wave = FindWaveDefinitionForRound(roundNumber);
         if (wave.Equals(null)) {
+            clearResourceDisplay();
             return;
         }
         spawning = true;
         currentWave = wave;
+        setupResourceDisplay(wave);
+    }
+
+    private WaveDefinition<ResourceType> FindWaveDefinitionForRound(int roundNumber) {
+        return waves.OrderByDescending(wave => wave.RoundToSpawnIn).FirstOrDefault(wave => wave.RoundToSpawnIn <= roundNumber);
+    }
+
+    private void setupResourceDisplay(WaveDefinition<ResourceType> wave) {
+        clearResourceDisplay();
+        resourceDisplay = Instantiate(wave.TypeToSpawn.Prefab, transform.position + Vector3.up * resourceDisplayHeight, Quaternion.identity, transform);
+        Destroy(resourceDisplay.GetComponent<Resource>());
+        Destroy(resourceDisplay.GetComponentInChildren<Interactable>().gameObject);
+        resourceDisplay.transform.localScale = Vector3.one * 0.5f;
+        // at this point the destroyed components above are not yet cleaned up, so we still find the interactable renderer
+        var renderers = resourceDisplay.GetComponentsInChildren<MeshRenderer>();
+        foreach (var renderer in renderers) {
+            renderer.material = resourceDisplayMaterial;
+        }
+    }
+
+    private void clearResourceDisplay() {
+        Destroy(resourceDisplay);
     }
 
     internal void StopSpawning() {
@@ -65,6 +101,6 @@ public class ResourceSpawner : MonoBehaviour {
     }
 
     public void OnDrawGizmos() {
-        Gizmos.DrawWireCube(transform.position, Vector3.one);
+        Gizmos.DrawWireCube(transform.position + Vector3.up * 0.5f, Vector3.one);
     }
 }
